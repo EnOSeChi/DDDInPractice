d50af704fbcf838bea181eaaffbc1ef58d21f967
Sławomir Hus <slawomir.hus@sybillatechnologies.com>
2021-02-25 13:30:16 +0100

    base value object class

--- DDDInPractice.Logic/Money.cs
+++ DDDInPractice.Logic/Money.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 namespace DDDInPractice.Logic
 {
-    public sealed class Money
+    public sealed class Money : ValueObject
     {
         public Money(
             int oneCentCount,
             int tenCentCount,
             int quarterCount,
-            int onDollarCount,
+            int oneDollarCount,
             int fiveDollarCount,
             int twentyDollarCount)
         {
             OneCentCount = oneCentCount;
             TenCentCount = tenCentCount;
             QuarterCount = quarterCount;
-            OnDollarCount = onDollarCount;
+            OneDollarCount = oneDollarCount;
             FiveDollarCount = fiveDollarCount;
             TwentyDollarCount = twentyDollarCount;
         }
@@ -25,9 +25,19 @@ namespace DDDInPractice.Logic
         public int OneCentCount { get; private set; }
         public int TenCentCount { get; private set; }
         public int QuarterCount { get; private set; }
-        public int OnDollarCount { get; private set; }
+        public int OneDollarCount { get; private set; }
         public int FiveDollarCount { get; private set; }
         public int TwentyDollarCount { get; private set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return OneCentCount;
+            yield return TenCentCount;
+            yield return QuarterCount;
+            yield return OneDollarCount;
+            yield return FiveDollarCount;
+            yield return TwentyDollarCount;
+        }
 
         public static Money operator +(Money money1, Money money2)
         {
@@ -35,7 +45,7 @@ namespace DDDInPractice.Logic
                 money1.OneCentCount += money2.OneCentCount,
                 money1.TenCentCount += money2.TenCentCount,
                 money1.QuarterCount += money2.QuarterCount,
-                money1.OnDollarCount += money2.OnDollarCount,
+                money1.OneDollarCount += money2.OneDollarCount,
                 money1.FiveDollarCount += money2.FiveDollarCount,
                 money1.TwentyDollarCount += money2.TwentyDollarCount);
 
