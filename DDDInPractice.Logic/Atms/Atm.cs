ec9912da6478929b44231e0c4d497b8d80277ce7
EnOSeChi <slawekhus@gmail.com>
2021-04-08 17:28:10 +0200

    calculate amount with commission

--- DDDInPractice.Logic/Atms/Atm.cs
+++ DDDInPractice.Logic/Atms/Atm.cs
@@ -6,6 +6,7 @@ namespace DDDInPractice.Logic.Atms
 {
     public class Atm : AggregateRoot
     {
+        private const decimal CommisionRate = 0.01m;
         public virtual Money MoneyInside { get; protected set; } = Money.None;
         public virtual decimal MoneyCharged { get; protected set; }
 
@@ -13,8 +14,19 @@ namespace DDDInPractice.Logic.Atms
         {
             Money output = MoneyInside.Allocate(amount);
             MoneyInside -= output;
-            decimal amountWithCommission = amount + amount * 0.01m;
+            decimal amountWithCommission = CalculateAmountWithCommission(amount);
             MoneyCharged += amountWithCommission;
+        }
+
+        private decimal CalculateAmountWithCommission(decimal amount)
+        {
+            decimal commision = amount * CommisionRate;
+            decimal lessThanCent = commision % 0.01m;
+            if (lessThanCent > 0)
+            {
+                commision = commision - lessThanCent + 0.01m;
+            }
+            return amount + commision;
         }
 
         public virtual void LoadMoney(Money money)
