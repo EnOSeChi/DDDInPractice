70f3e5018720725b258e8b291c1883efd2235ee3
EnOSeChi <slawekhus@gmail.com>
2021-04-08 17:22:18 +0200

    adding atm aggregate root

---
+++ DDDInPractice.Logic/Atms/Atm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Logic.Atms
+{
+    public class Atm : AggregateRoot
+    {
+        public virtual Money MoneyInside { get; protected set; }
+        public virtual decimal MoneyCharged { get; protected set; }
+
+        public virtual void TakeMoney(decimal amount)
+        {
+
+        }
+    }
+}
