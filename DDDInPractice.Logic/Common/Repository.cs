e2d5ba7c758b9329732c19d8907aaa0f2626797b
EnOSeChi <slawekhus@gmail.com>
2021-04-08 17:16:27 +0200

    new folder structure

---
+++ DDDInPractice.Logic/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using DDDInPractice.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Application
+{
+    public class Repository<T> where T : AggregateRoot
+    {
+        public T GetById(long id)
+        {
+            using (var ctx = ContextFactory.DefaultContext())
+            {
+                return ctx.Find<T>(id);
+            }
+        }
+
+        public void Save(T aggregateRoot)
+        {
+            using (var ctx = ContextFactory.DefaultContext())
+            using(var tran = ctx.Database.BeginTransaction())
+            {
+                ctx.Update(aggregateRoot);
+                ctx.SaveChanges();
+                tran.Commit();
+            }
+        }
+    }
+}
