54825fd33cf4150ca633b2ad650858ffdc788c2d
EnOSeChi <slawekhus@gmail.com>
2021-04-21 14:38:45 +0200

    adding management bounded context

---
+++ DDDInPractice.Logic/Management/HeadOffice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Logic.Management
+{
+    public class HeadOffice : AggregateRoot
+    {
+        public virtual decimal Balance { get; set; }
+        public virtual Money Cash { get; set; }
+    }
+}
