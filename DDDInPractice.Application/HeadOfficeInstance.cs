ab29cfc08fb33c8d48f0731fcb83518f3091b2b6
EnOSeChi <slawekhus@gmail.com>
2021-04-21 14:57:54 +0200

    adding head office instance

---
+++ DDDInPractice.Application/HeadOfficeInstance.cs
@@ -0,0 +1,20 @@
+﻿using DDDInPractice.Logic.Management;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Application
+{
+    public static class HeadOfficeInstance
+    {
+        private const long HeadOfficeId = 1;
+
+        public static HeadOffice Instance { get; private set; }
+
+        public static void Init()
+        {
+            var repository = new HeadOfficeRepository();
+            Instance = repository.GetById(HeadOfficeId);
+        }
+    }
+}
