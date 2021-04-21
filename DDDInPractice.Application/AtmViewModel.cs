2b93145f031e0f08761454f21ac65fc61b174a36
EnOSeChi <slawekhus@gmail.com>
2021-04-21 14:43:11 +0200

    atm view model

---
+++ DDDInPractice.Application/AtmViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DDDInPractice.Logic.Atms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Application
+{
+    public class AtmViewModel
+    {
+        private Atm _atm;
+
+        public AtmViewModel(Atm atm)
+        {
+            _atm = atm;
+        }
+
+        private void TakeMoney(decimal amount)
+        {
+            _atm.TakeMoney(amount);
+            NotifyClient($"You have taken {amount.ToString("C2")}");
+        }
+
+        private void NotifyClient(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
