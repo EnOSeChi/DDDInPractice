22080d11e174b3bac584524f78ff8f134614f601
EnOSeChi <slawekhus@gmail.com>
2021-04-21 14:55:27 +0200

    head office repository

---
+++ DDDInPractice.Application/HeadOfficeRepository.cs
@@ -0,0 +1,11 @@
+﻿using DDDInPractice.Logic.Management;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInPractice.Application
+{
+    public class HeadOfficeRepository : Repository<HeadOffice>
+    {
+    }
+}
